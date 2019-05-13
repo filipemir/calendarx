890cf7c8d7f01d07df360b3e9493ac105d16f005
fmiranda <fmiranda@smartertravelmedia.com>
2019-05-13 14:52:10 -0400

    Another try

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ import * as React from "react";
 import moment from "moment";
 
 // export = CalendarX; 
-export declare class Waypoint extends React.FunctionComponent<CalendarxProps> {
+export declare class Waypoint implements React.FunctionComponent<CalendarxProps> {
     useCalendar: (props: CalendarxProps) => CalendarxChildrenProps;
 };
 
